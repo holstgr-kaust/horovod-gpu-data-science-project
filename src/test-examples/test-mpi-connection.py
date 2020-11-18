3fe78dd47c58cbc7c0329ad1623364c46366cfdd
Glendon Holst <glendon.holst@kaust.edu.sa>
2020-11-18 12:45:43 +0300

    Fixed method reference typo

--- src/test-examples/test-mpi-connection.py
+++ src/test-examples/test-mpi-connection.py
@@ -23,7 +23,7 @@ print('hvd.inited - initialized:', hvd.is_initialized(),
       ' local rank:', hvd.local_rank(), ' global rank:', hvd.rank(), 
       ' local ranks:', hvd.local_size(), ' global ranks:', hvd.size())
 print('hvd built - mpi_built:', hvd.mpi_built(), 
-      ' nccl_built:', hvd.nccl_built(), ' cuda_built:', cuda_built())
+      ' nccl_built:', hvd.nccl_built(), ' cuda_built:', hvd.cuda_built())
 print('hvd MPI - mpi_enabled:', hvd.mpi_enabled(), 
       ' mpi_threads_supported:', hvd.mpi_threads_supported())
 
